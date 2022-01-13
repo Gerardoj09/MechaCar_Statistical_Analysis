f98c3355ed00d0f507a98400a135b5e09902bba2
Gerardo Jiménez <88596274+Gerardoj09@users.noreply.github.com>
2022-01-12 22:50:57 -0600

    Ch15

---
+++ MechaCarChallenge.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+df <- read.csv(file='MechaCar_mpg.csv')
+head(df, n=10)
+
+lm(formula =  mpg ~ vehicle_weight + vehicle_length + spoiler_angle + ground_clearance + AWD, data = df) #generate multiple linear regression model
+
+summary(lm(formula =  mpg ~ vehicle_weight + vehicle_length + spoiler_angle + ground_clearance + AWD, data = df))
+
+df_coils <- read.csv(file='Suspension_Coil.csv')
+head(df, n=10)
+
+total_summary <- df_coils %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- df_coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
